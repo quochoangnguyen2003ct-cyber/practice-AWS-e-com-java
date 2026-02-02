6f01d75db2302fcf725ad823b140ce9af74ba418
WIN-6BOU8GF8IFA\admin <quochoangnguyen2003ct@gmail.com>
2026-02-02 22:49:55 +0700

    Add CI

---
+++ demo/src/main/java/com/example/demo/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Spring Boot is running!";
+    }
+    @GetMapping("/hello")
+    public String helloString() {
+        return "Hello CI/CD";
+    }
+}
